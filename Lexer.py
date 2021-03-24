db8ca8058e107daffb58ec965d9a398ac0af8845
Danilo <unifdaoliveira@fei.edu.br>
2021-03-24 18:25:16 -0300

    add symbol module
    also update lexer
    remove old afd files
    update .gitignore
    update token module to include old tokentype class

--- Lexer.py
+++ Lexer.py
@@ -1,3 +1,12 @@
+"""
+Danilo Bizarria
+Kaike Rodrigues
+Markel Duarte
+Matheus Ferreira
+Rafael Lino
+"""
+
+"""
 # Parser(self, Lexer):
 #     while(Lexer.hasNextToken()):
 
@@ -12,33 +21,102 @@
 #     ("2", NUMBER)
 # ]
 
-import TokenType
+int a = 10;
+
+Symbol(Token tk, Type )
+
+Token("a", IDENTIFIER, ...)
+type 
+"""
+
+
+from enum import Enum
 
 
 class Lexer:
     def __init__(self, code):
         self.code = code
-        self.charIndex = 0
+        self.tokens = []
+        self.tokenStart = 0
+        self.nextIndex = 0
         self.line = 1
 
-    # uso externo
-    def hasNextToken(self):
+
+    # iterator methods
+    def __iter__(self):
+        return self
+
+
+    def __next__(self):
+        return self.get_token()
+   
+
+    def has_next_char(self):
+        return self.nextIndex < len(code)
+
+
+    def next_char(self):
+        if not self.has_next_char():
+            return None
+        
+        c = self.code[nextIndex]
+        self.nextIndex += 1
+        return c
+
+
+    def is_next_char(self, c):
+        if not self.has_next_char():
+            return False
+       
+        if self.code[self.nextIndex] == c:
+            self.nextIndex += 1
+            return True
+        
         return False
 
-    def nextToken(self):
-        return None
 
     def switch_char(self, c):
         cases = {
-                '(': None,
-                ')': None,
-                '{': None
-                }
-        return cases.get(c)
+            '(': lambda: self.push_token(LEFT_PAREN),
+            ')': lambda: self.push_token(RIGHT_PAREN),
+            '{': lambda: self.push_token(LEFT_BRACE),
+            '}': lambda: self.push_token(RIGHT_BRACE),
+            ',': lambda: self.push_token(COMMA),
+            '.': lambda: self.push_token(DOT),
+            '-': lambda: self.push_token(MINUS),
+            '+': lambda: self.push_token(PLUS),
+            ';': lambda: self.push_token(SEMICOLON),
+            '*': lambda: self.push_token(STAR),
+            '?': lambda: self.push_token(QUESTION),
+            ':': lambda: self.push_token(COLON),
+                
+            '/': lambda: self.push_token(NOT_EQUAL if is_next_char("=") else SLASH),
+            '=': lambda: self.push_token(EQUAL_EQUAL if is_next_char("=") else EQUAL),
+            '>': lambda: self.push_token(GREATER_EQUAL if is_next_char("=") else GREATER),
+            '<': lambda: self.push_token(LESS_EQUAL if is_next_char("=") else LESS),
+            ' ': lambda: None,
+            '\n': lambda: self.line += 1
+              return None
+            '': None
+        }
 
+        return cases[c]
+
+
+    def get_token(self):
+        c = self.next_char()
+        token = switch_char(c)
+        
+        # whitespace
+        if Token is None:
+            token = self.get_token()
+
+        return token
+
+            
     def getTokens(self):
-        while(self.hasNextChar()):
-            c = self.nextChar()
+        while(self.has_next_char()):
+            c = self.next_char()
             return self.switch_char(c)
 
 # if(x>0){a+=10;}
---
+++ symbol.py
@@ -0,0 +1,43 @@
+'''
+{
+int a = 10;
+}
+
+Token tk_a = Token("a", TokenType.IDENTIFIER, ...)
+Symbol(tk_a, LangType.Int, ScopeType.Local)
+
+LangType.Int
+scope = local|global
+
+Token - Linha do codigo, lexema, tokenType
+
+X = symbol(TOK, int, global)
+
+'''
+
+
+import token
+
+
+class Symbol():
+    def __init__(self, token, s_type, scope):
+        self.token = token
+        self.s_type = s_type
+        self.scope = scope
+
+
+class SymbolTable():
+    def __init__(self):
+        # initialize table itself
+        self.table = {}
+
+
+    def insert(self, sym, name):
+        table[name] = sym
+
+        return sym
+
+
+    def lookup(self, name):
+        return table[name]
+
